cb3a1df45eda430856fd2b7506005d7141d1e9b0
persianpros <persianpros@yahoo.com>
2020-05-08 21:02:53 +0430

    Backport cdparanoia from Open Vision 9.3

--- update.sh
+++ update.sh
@@ -80,6 +80,7 @@ rm -rf meta-openpli/recipes-core/busybox
 rm -rf meta-openpli/recipes-extended/tzdata
 rm -rf meta-openpli/recipes-linux
 rm -rf meta-openpli/recipes-mediacenter
+rm -rf meta-openpli/recipes-multimedia/cdparanoia
 rm -rf meta-openpli/recipes-multimedia/dvblast
 rm -rf meta-openpli/recipes-multimedia/tuxbox
 rm -rf meta-openpli/recipes-multimedia/tuxtxt
